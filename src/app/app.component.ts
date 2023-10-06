bbdfcebd76ba8e53d74e161a155de43f8ffe050f
Scott Danzig <2577300+sdanzig@users.noreply.github.com>
2023-10-05 23:00:57 -0400

    Fix UI to have just a button and allow multiple uploads of the same file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,48 @@ import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
-  template: `<input type="file" (change)="onFileChange($event)">`
+  template: `
+    <button (click)="fileInput.click()">Upload File</button>
+    <input #fileInput type="file" style="display: none" (change)="onFileChange($event)">
+    <div *ngIf="uploading">
+      Uploading...
+    </div>
+  `,
+  styles: [`
+    button {
+      padding: 10px;
+      background-color: #007bff;
+      color: white;
+      border: none;
+      border-radius: 4px;
+    }
+  `]
 })
 export class AppComponent {
+  uploading = false;
+
   constructor(private userService: UserService) {}
 
   onFileChange(event: any) {
+    if (event.target.files.length === 0) {
+      console.log("No file selected");
+      return;
+    }
+  
+    this.uploading = true;
+    
     const file = event.target.files[0];
     this.userService.uploadFile(file).subscribe({
-      next: () => console.log('Upload successful'),
-      error: () => console.log('Upload failed')
+      next: () => {
+        this.uploading = false;
+        console.log('Upload successful');
+        event.target.value = null;
+      },
+      error: () => {
+        this.uploading = false;
+        console.log('Upload failed');
+        event.target.value = null;
+      }
     });
   }
 }
